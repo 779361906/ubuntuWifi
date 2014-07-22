4438ccdae915f3751e12736686446ecd81fac4b7
779361906 <wu779361906@gmail.com>
2014-07-22 21:26:02 +0800

    update bash-wifi.sh

--- bash-wifi.sh
+++ bash-wifi.sh
@@ -2,31 +2,17 @@
 
 psk=\""$2"\"
 ssid=\""$3"\"
-str=$(wpa_cli -iwlan0 disconnect)
-echo $str
-str=$(for i in `wpa_cli list_networks | grep ^[0-9] | cut -f1`; do wpa_cli -iwlan0 remove_network $i; done)
-echo $str
-str=$(wpa_cli -iwlan0 add_network)
-echo $str
-str=$(wpa_cli -iwlan0 set_network 0 auth_alg OPEN)
-echo $str
-str=$(wpa_cli -iwlan0 set_network 0 key_mgmt "$1")
-echo $str
-str=$(wpa_cli -iwlan0 set_network 0 psk $psk)
-echo $str
-str=$(wpa_cli -iwlan0 set_network 0 mode 0)
-echo $str
-str=$(wpa_cli -iwlan0 set_network 0 ssid $ssid)
-echo $str
-str=$(wpa_cli -iwlan0 select_network 0)
-echo $str
-str=$(wpa_cli -iwlan0 enable_network 0)
-echo $str
-str=$(wpa_cli -iwlan0 reassociate)
-echo $str
-str=$(wpa_cli -iwlan0 status)
-echo $str
-str=$(iw wlan0 link)
-echo $str
-str=$(dhclient wlan0)
-echo $str
+wpa_cli -iwlan0 disconnect
+for i in `wpa_cli list_networks | grep ^[0-9] | cut -f1`; do wpa_cli -iwlan0 remove_network $i; done
+wpa_cli -iwlan0 add_network
+wpa_cli -iwlan0 set_network 0 auth_alg OPEN
+wpa_cli -iwlan0 set_network 0 key_mgmt "$1"
+wpa_cli -iwlan0 set_network 0 psk $psk
+wpa_cli -iwlan0 set_network 0 mode 0
+wpa_cli -iwlan0 set_network 0 ssid $ssid
+wpa_cli -iwlan0 select_network 0
+wpa_cli -iwlan0 enable_network 0
+wpa_cli -iwlan0 reassociate
+wpa_cli -iwlan0 status
+iw wlan0 link
+dhclient wlan0
